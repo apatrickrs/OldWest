7d0a9ccac70a01c823637b76aaa5009177cc7f91
Andrew Patrick <andrew.prsilva@gmail.com>
2020-01-29 20:47:29 -0300

    Exception at findById

---
+++ src/main/java/com/oldwest/project/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.oldwest.project.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.oldwest.project.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+
+		String error = "Error not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError erro = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(erro);
+	}
+
+}
